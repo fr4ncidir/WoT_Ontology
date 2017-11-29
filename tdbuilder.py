c46089d28efb7095bbeedf3490c9e29625171fe0
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2017-11-29 23:27:51 +0100

    missing ontology check for errors, and unexpected characters removing in SPARQL code

--- tdbuilder.py
+++ tdbuilder.py
@@ -23,30 +23,56 @@
 #  
 
 from shutil import copy
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as etree
 import json
 
 TD_template = "./thing_description.jsap"
 TD_complete = "./thing_description2.jsap"
-ontology_namespace = "http://wot.arces.unibo.it/ontology/web_of_things#"
-namespaces = {"owl","http://www.w3.org/2002/07/owl#"}
+sepa_annotations = "http://www.semanticweb.org/francesco/ontologies/2017/9/sepa_annotations#"
+
+ns = {	"owl"			:"http://www.w3.org/2002/07/owl#",
+		"web_of_things"	:"http://wot.arces.unibo.it/ontology/web_of_things#",
+		"rdf"			:"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+		"rdfs"			:"http://www.w3.org/2000/01/rdf-schema#"}
+		
+def jsap_build(entry,jsap_level,json_data,jsap_file):
+	entry_data = entry.replace("\n","").split(":",1)
+	print("{}-----{}".format(entry_data[0],entry_data[1]))
+	j_key = entry[1:-1]
+	j_object = json.loads(entry_data[1])
+	json_data[jsap_level][j_key]=j_object
+	jsap_file.seek(0)
+	json.dump(json_data,jsap_file,indent=4)
+	jsap_file.truncate()
 
 def main(args):
 	copy(TD_template,TD_complete)
 	
-	owl = ET.parse("./td.owl")
-	root = owl.getroot()
-	for element in root.findall("{http://www.w3.org/2002/07/owl#}AnnotationProperty"):
-		for item in element.attrib.values():
-			print("-->{}".format(item))
+	tree = etree.parse("./td.owl")
+	root = tree.getroot()
+	queries = []
+	updates = []
+	for element in root.findall("owl:AnnotationProperty",ns):
+		for annotation_type in element.findall("rdfs:subPropertyOf",ns):
+			#print("{} {}".format(annotation_type.tag,annotation_type.attrib))
+			annotation_type_string = annotation_type.attrib["{}{}".format("{"+ns["rdf"]+"}","resource")]
+			#print(annotation_type_string)
+			if (annotation_type_string==(sepa_annotations+"discovery")) or (annotation_type_string==(sepa_annotations+"query")):
+				queries.append(element.attrib["{}{}".format("{"+ns["rdf"]+"}","about")].replace(ns["web_of_things"],"web_of_things:"))
+			elif annotation_type_string==(sepa_annotations+"update"):
+				updates.append(element.attrib["{}{}".format("{"+ns["rdf"]+"}","about")].replace(ns["web_of_things"],"web_of_things:"))
+	
+	
 	
 	with open(TD_complete,"r+") as jsap:
 		data = json.load(jsap)
-		data["queries"]["francesco"]="ciao"
-		data["queries"]["wilbur"]="smith"
-		jsap.seek(0)
-		json.dump(data,jsap,indent=4)
-		jsap.truncate()
+		for element in queries:
+			for item in root.findall(".//"+element,ns):
+				jsap_build(item.text,"queries",data,jsap)
+		for element in updates:
+			for item in root.findall(".//"+element,ns):
+				print(item.text)
+				jsap_build(item.text,"updates",data,jsap)
 	return 0
 
 if __name__ == '__main__':
