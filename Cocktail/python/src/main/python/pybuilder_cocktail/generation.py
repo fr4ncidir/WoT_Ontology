e7dfdf0cc59c10a766ed033946a86833e10f8935
relu <relu.cri@gmail.com>
2018-02-05 16:52:59 +0100

    Fix data loading for template
    Decoding data readed from resources with
    right code.

--- Cocktail/python/src/main/python/pybuilder_cocktail/generation.py
+++ Cocktail/python/src/main/python/pybuilder_cocktail/generation.py
@@ -7,7 +7,7 @@ _resource_package = 'pybuilder_cocktail' # Could be any module/package name
 _resource_path = '/'.join(('templates', 'thing.tpl'))
 
 _template_data = pkg_resources.resource_string(_resource_package, _resource_path)
-_template = Template(str(_template_data))
+_template = Template(_template_data.decode("cp1252"))
 
 def generate(src,wt):
     path = "%s/%s.py" % (src,wt.name)
